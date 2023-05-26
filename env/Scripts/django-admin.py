b92824a86e8f376023c8835ffbd8dab00dc7bd79
cflfung <cflfung@gmail.com>
2023-05-27 00:03:49 +0800

    my first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\sherry\Documents\tutorials\django_lectures\carzone-project\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
